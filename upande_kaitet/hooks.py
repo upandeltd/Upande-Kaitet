17dda15ab7d907f0dda16435911ab22ed2272f99
Anita Wangui <anitawangui22@gmail.com>
2025-07-08 17:42:41 +0300

    fix item price naming system

--- upande_kaitet/hooks.py
+++ upande_kaitet/hooks.py
@@ -132,7 +132,9 @@ app_license = "mit"
 # override_doctype_class = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
-
+override_doctype_class = {
+    "Item Price": "upande_kaitet.overrides.custom_item_price_naming.CustomItemPrice"
+}
 # Document Events
 # ---------------
 # Hook on document methods and events
