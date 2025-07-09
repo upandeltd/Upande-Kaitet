aabb08a55294b34646f10b0152971731867d565f
rod1gers <rodgersochieng143@gmail.com>
2025-07-09 17:15:04 +0300

    Fix publisher error

--- upande_kaitet/upande_kaitet/doctype/specification/test_specification.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, 	Upande and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSpecification(FrappeTestCase):
-	pass
